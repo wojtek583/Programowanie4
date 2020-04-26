4d59b0d7860edce9c701ce750afed3c40d1408e6
Wojciech <wojtek583@op.pl>
2020-04-26 23:40:13 +0200

    Zombie Apocalypse alphfav0.01

---
+++ ZombieApocalypse/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ZombieApocalypse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn1Exit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btn1Exit
+            // 
+            this.btn1Exit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn1Exit.AutoSize = true;
+            this.btn1Exit.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn1Exit.Location = new System.Drawing.Point(413, 270);
+            this.btn1Exit.Name = "btn1Exit";
+            this.btn1Exit.Size = new System.Drawing.Size(54, 23);
+            this.btn1Exit.TabIndex = 5;
+            this.btn1Exit.Text = "Wyjście";
+            this.btn1Exit.UseVisualStyleBackColor = true;
+            this.btn1Exit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 26);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Rozpocznij grę!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ilość ludzi";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Yellow;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(90, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 13);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(90, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 13);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Ilość wojska";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Green;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(90, 79);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(75, 13);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Ilość zombie";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(314, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Copyright © 2020 MocuGames Inc. Wszelkie prawa zastrzeżone.";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Silver;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(15, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 55);
+            this.label5.TabIndex = 13;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(479, 305);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn1Exit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Zombie Apocalypse by MocuGames";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn1Exit;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ZombieApocalypse/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//  Projekt do dokonczenia
+//  made by Wojciech Moc
+//  2020
+//  https://github.com/wojtek583/Programowanie4 - Zombie Apocalypse
+
+namespace ZombieApocalypse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            this.Size = new System.Drawing.Size(495,344);
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+        }
+        PictureBox[,] Box;
+        int czlowiekX, czlowiekY, ludzie, wojsko, zombie, tura;
+        Random rnd = new Random();
+           
+        
+        public void button1_Click(object sender, EventArgs e)
+        {
+            
+            if(int.TryParse(textBox1.Text, out ludzie)!=true || int.TryParse(textBox2.Text, out wojsko) != true || int.TryParse(textBox3.Text, out zombie) != true || textBox1.Text==string.Empty)
+            {
+                MessageBox.Show("Musisz wpisac liczbe calkowita!");
+            }
+            else
+            {
+                timer1.Start();
+                timer1.Interval = 5000;
+                textBox1.ReadOnly = true;
+                textBox2.ReadOnly = true;
+                textBox3.ReadOnly = true;
+                plansza(czlowiekX,czlowiekY);
+                label5.Text = $"Ludzie:\t  { textBox1.Text} \nWojsko:\t { textBox2.Text} \nZombie:\t { textBox3.Text} \nIlosc tur: {tura}";
+
+                
+                timer1.Tick += Timer1_Tick;
+                
+            }
+            
+        }
+     
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            tura++;
+            
+        }
+
+        public void plansza(int x, int y)
+        {
+            Box = new PictureBox[30, 30];
+            for (int i = 0; i < 25; ++i)
+            {
+                for (int j = 0; j < 25; j++)
+                {
+                    Box[i, j] = new PictureBox();
+                    Box[i, j].Top = 12 + i * 10;
+                    Box[i, j].Left = 217 + j * 10;
+                    Box[i, j].Width = 10;
+                    Box[i, j].Height = 10;
+                    Box[i, j].BackColor = Color.DarkCyan;
+                    Box[i, j].BorderStyle = BorderStyle.FixedSingle;
+                    Controls.Add(Box[i, j]);
+                }
+            }
+            for (int i = 0; i < ludzie; i++)
+            {
+                
+                Box[rnd.Next(0, 25), rnd.Next(0, 25)].BackColor = Color.Yellow;
+            }
+            for (int i = 0; i < zombie; i++)
+            {
+                Box[rnd.Next(0, 25), rnd.Next(0, 25)].BackColor = Color.Green;
+            }
+            for (int i = 0; i < wojsko; i++)
+            {
+
+                Box[rnd.Next(0, 25), rnd.Next(0, 25)].BackColor = Color.Orange;
+            }
+            /*    
+             *    
+             *    Albo obsluka click albo tooltip
+            ToolTip tool = new ToolTip();
+            tool.SetToolTip(Box[1, 1],"To jest pierwszy klocuch");
+            */
+            
+            for (int i = 0; i < 25; ++i)
+            {
+                for (int j = 0; j < 25; j++)
+                {
+                    Box[i, j].Click += Form1_Click;
+                    //Info(Box[1, 1]);
+                }
+            }
+            
+
+        }
+
+        private void Info(PictureBox box)
+        {
+            if (box.BackColor==Color.Yellow)
+            {
+                MessageBox.Show("Zycie:\nPieniadze:", "Czlowiek");
+            }
+            if (box.BackColor == Color.Green)
+            {
+                MessageBox.Show("Zycie:\nPieniadze:\nLiczba Tur:", "Zombie");
+            }
+            if (box.BackColor == Color.Orange)
+            {
+                MessageBox.Show("Zycie:\nPieniadze:", "Wojsko");
+            }
+        }
+      
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (BackColor == Color.Yellow)
+            {
+                MessageBox.Show("Zycie:\nPieniadze:", "Czlowiek");
+            }
+            else MessageBox.Show("dziala?");
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Buton");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            this.Text = String.Empty;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
