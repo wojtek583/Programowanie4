9113433a7d887b905e3f3e0368a58bc00ef87387
Wojciech <wm052943@student.ath.edu.pl>
2020-04-28 13:51:02 +0200

    Chanched pictureboxes to buttons, Used tags instead of array of boxes, Refactor code -> now it's more clearly.

--- ZombieApocalypse/Form1.cs
+++ ZombieApocalypse/Form1.cs
@@ -7,10 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//  Projekt do dokonczenia
-//  made by Wojciech Moc
-//  2020
-//  https://github.com/wojtek583/Programowanie4 - Zombie Apocalypse
+        //  Project not finished yet
+        //  Made by Wojciech Moc
+        //  April, 2020
+        //  https://github.com/wojtek583/Programowanie4 - Zombie Apocalypse
 
 namespace ZombieApocalypse
 {
@@ -19,130 +19,88 @@ namespace ZombieApocalypse
         public Form1()
         {
             InitializeComponent();
-            
-            this.Size = new System.Drawing.Size(495,344);
+
+            this.Size = new System.Drawing.Size(495, 344);
             this.MaximumSize = this.Size;
             this.MinimumSize = this.Size;
         }
-        PictureBox[,] Box;
-        int czlowiekX, czlowiekY, ludzie, wojsko, zombie, tura;
+
+        int people, soldiers, zombies;
+        int round=0;
         Random rnd = new Random();
-           
-        
+
         public void button1_Click(object sender, EventArgs e)
         {
-            
-            if(int.TryParse(textBox1.Text, out ludzie)!=true || int.TryParse(textBox2.Text, out wojsko) != true || int.TryParse(textBox3.Text, out zombie) != true || textBox1.Text==string.Empty)
+
+            if (int.TryParse(textBox1.Text, out people) != true || int.TryParse(textBox2.Text, out soldiers) != true || int.TryParse(textBox3.Text, out zombies) != true || textBox1.Text == string.Empty)
             {
-                MessageBox.Show("Musisz wpisac liczbe calkowita!");
+                MessageBox.Show("Must be non-decimal!");
             }
             else
             {
-                timer1.Start();
-                timer1.Interval = 5000;
+                map();
                 textBox1.ReadOnly = true;
                 textBox2.ReadOnly = true;
                 textBox3.ReadOnly = true;
-                plansza(czlowiekX,czlowiekY);
-                label5.Text = $"Ludzie:\t  { textBox1.Text} \nWojsko:\t { textBox2.Text} \nZombie:\t { textBox3.Text} \nIlosc tur: {tura}";
-
-                
-                timer1.Tick += Timer1_Tick;
-                
+                label5.Text = $"People:\t  { textBox1.Text} \nSoldiers:\t { textBox2.Text} \nZombies:\t { textBox3.Text} \nRounds: {round}";
             }
-            
+
         }
-     
+
         private void Timer1_Tick(object sender, EventArgs e)
         {
-            tura++;
-            
+            round++;
         }
 
-        public void plansza(int x, int y)
+        public void map()
         {
-            Box = new PictureBox[30, 30];
-            for (int i = 0; i < 25; ++i)
+            for (int i = 0; i < 25; i++)
             {
                 for (int j = 0; j < 25; j++)
                 {
-                    Box[i, j] = new PictureBox();
-                    Box[i, j].Top = 12 + i * 10;
-                    Box[i, j].Left = 217 + j * 10;
-                    Box[i, j].Width = 10;
-                    Box[i, j].Height = 10;
-                    Box[i, j].BackColor = Color.DarkCyan;
-                    Box[i, j].BorderStyle = BorderStyle.FixedSingle;
-                    Controls.Add(Box[i, j]);
+                    var btn = new Button
+                    {
+                        Text = (i + j).ToString(),
+                        Width = 10,
+                        Height = 10,
+                        Top = 12 + i * 10,
+                        Left = 217 + j * 10,
+                        Tag = (i, j),
+                        Parent = this
+                    };
+                    btn.Click += Form1_Click;
                 }
             }
-            for (int i = 0; i < ludzie; i++)
-            {
-                
-                Box[rnd.Next(0, 25), rnd.Next(0, 25)].BackColor = Color.Yellow;
-            }
-            for (int i = 0; i < zombie; i++)
-            {
-                Box[rnd.Next(0, 25), rnd.Next(0, 25)].BackColor = Color.Green;
-            }
-            for (int i = 0; i < wojsko; i++)
-            {
-
-                Box[rnd.Next(0, 25), rnd.Next(0, 25)].BackColor = Color.Orange;
-            }
-            /*    
-             *    
-             *    Albo obsluka click albo tooltip
-            ToolTip tool = new ToolTip();
-            tool.SetToolTip(Box[1, 1],"To jest pierwszy klocuch");
-            */
-            
-            for (int i = 0; i < 25; ++i)
-            {
-                for (int j = 0; j < 25; j++)
-                {
-                    Box[i, j].Click += Form1_Click;
-                    //Info(Box[1, 1]);
-                }
-            }
-            
-
         }
-
-        private void Info(PictureBox box)
+        public class Human
         {
-            if (box.BackColor==Color.Yellow)
-            {
-                MessageBox.Show("Zycie:\nPieniadze:", "Czlowiek");
-            }
-            if (box.BackColor == Color.Green)
-            {
-                MessageBox.Show("Zycie:\nPieniadze:\nLiczba Tur:", "Zombie");
-            }
-            if (box.BackColor == Color.Orange)
-            {
-                MessageBox.Show("Zycie:\nPieniadze:", "Wojsko");
-            }
+            public int coordX { get; set; }
+            public int coordY { get; set; }
+            public int Pieniadze { get; set; }
         }
-      
+        public class Zombie
+        {
+            public int coordX { get; set; }
+            public int coordY { get; set; }
+            public int Rounds { get; set; }
+            public int Strength { get; set; }
+        }
+        public class Soldier
+        {
+            public int coordX { get; set; }
+            public int coordY { get; set; }
+            public int Money { get; set; }
+            public int Durability { get; set; }
+        }
         private void Form1_Click(object sender, EventArgs e)
         {
-            if (BackColor == Color.Yellow)
-            {
-                MessageBox.Show("Zycie:\nPieniadze:", "Czlowiek");
-            }
-            else MessageBox.Show("dziala?");
-            
+            var (a, b) = ((int, int))(sender as Control).Tag;
+            MessageBox.Show($"x={a},  y={b}");
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-        }
-        private void Btn_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Buton");
-        }
 
+        }
         private void button2_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -150,18 +108,11 @@ namespace ZombieApocalypse
 
         private void label5_Click(object sender, EventArgs e)
         {
-            this.Text = String.Empty;
-        }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-            
-            
         }
-
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
     }
 }
